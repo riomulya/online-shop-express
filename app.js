5ca4650f2f431d58233223178dcb78327d3f37f4
RioMulya <riomulya75@gmail.com>
2023-05-28 08:47:26 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const db = require('./data/database');
+const authRoutes = require('./routes/auth.routes');
 
-app.listen(8080);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(authRoutes);
+
+db.connectToDatabase()
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch((e) => console.log('Failed to connect to the database : ', e));
